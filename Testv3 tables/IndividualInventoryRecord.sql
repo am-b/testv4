97718fd84ebc46a1822541802e1bf258b62d2599
Alyssa Bala <Alyssa Bala@MAJBALA>
2018-01-04 19:01:44 +0800

    UserID from Student table FK

--- Testv3 tables/IndividualInventoryRecord.sql
+++ Testv3 tables/IndividualInventoryRecord.sql
@@ -1,7 +1,7 @@
 USE [Testv3]
 GO
 
-/****** Object:  Table [dbo].[IndividualInventoryRecord]    Script Date: 12/23/2017 12:06:24 AM ******/
+/****** Object:  Table [dbo].[IndividualInventoryRecord]    Script Date: 1/4/2018 6:58:34 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -85,10 +85,5 @@ PRIMARY KEY CLUSTERED
 	[RecordID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[IndividualInventoryRecord]  WITH CHECK ADD FOREIGN KEY([UserID])
-REFERENCES [dbo].[Student] ([UserID])
-ON DELETE CASCADE
 GO
 
