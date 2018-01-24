290f8cbfc19df1277939f8a4fa70eef3b9a8dea9
Alyssa Bala <Alyssa Bala@MAJBALA>
2018-01-25 00:39:25 +0800

    Added chart.js references

--- Testv3/App_Start/BundleConfig.cs
+++ Testv3/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace Testv3
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/Chart.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
