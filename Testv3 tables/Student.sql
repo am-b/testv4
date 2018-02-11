f92373f9d5dcf5a8b91ad36718cc00d6f7233ba7
Alyssa Bala <Alyssa Bala@MAJBALA>
2018-02-11 22:18:43 +0800

    Removed tables from db

--- Testv3 tables/Student.sql
+++ Testv3 tables/Student.sql
@@ -1,7 +1,7 @@
 USE [Testv3]
 GO
 
-/****** Object:  Table [dbo].[Student]    Script Date: 1/4/2018 6:59:49 PM ******/
+/****** Object:  Table [dbo].[Student]    Script Date: 2/2/2018 8:25:53 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -15,7 +15,6 @@ CREATE TABLE [dbo].[Student](
 	[StudentFirstName] [nvarchar](50) NULL,
 	[StudentMiddleName] [nvarchar](50) NULL,
 	[StudentEmail] [nvarchar](256) NULL,
-	[CourseID] [int] NULL,
 	[Address] [nvarchar](256) NULL,
 	[Sex] [nchar](10) NULL,
 	[Civil Status
@@ -39,15 +38,13 @@ CivilStatus] [nchar](10) NULL,
 	[Occupation] [nvarchar](50) NULL,
 	[StudentEmployerAddress] [nvarchar](250) NULL,
 	[NumberOfChildren] [nvarchar](3) NULL,
+	[Program] [nvarchar](50) NULL,
+	[YearLevel] [int] NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[UserID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Student]  WITH CHECK ADD FOREIGN KEY([CourseID])
-REFERENCES [dbo].[Course] ([CourseID])
 GO
 
 ALTER TABLE [dbo].[Student]  WITH CHECK ADD FOREIGN KEY([UserID])
