686cc6654d11f1316abb5ca761b6ec9f11a18447
Alyssa Bala <Alyssa Bala@MAJBALA>
2018-05-14 14:52:22 +0800

    added jQuery validation

--- Testv3/App_Start/BundleConfig.cs
+++ Testv3/App_Start/BundleConfig.cs
@@ -8,19 +8,21 @@ namespace Testv3
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/admin-lte/js").Include(
-             "~/admin-lte/js/app.js",
-             "~/admin-lte/plugins/fastclick/fastclick.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery-ui-{version}.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/jquery.validate.unobtrusive.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/admin-lte/js").Include(
+             "~/admin-lte/js/app.js",
+             "~/admin-lte/plugins/fastclick/fastclick.js"));
+
+            
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
