ce884f94d5c5fc3b410294a856e07b07c48256c2
Alyssa Bala <Alyssa Bala@MAJBALA>
2018-06-02 19:24:39 +0800

    Email verif

--- Testv3/Controllers/AccountController.cs
+++ Testv3/Controllers/AccountController.cs
@@ -81,6 +81,13 @@ namespace Testv3.Controllers
                 return View(model);
             }
 
+            var userid = UserManager.FindByEmail(model.Email).Id;
+            if (!UserManager.IsEmailConfirmed(userid))
+            {
+                ModelState.AddModelError("", "Please confirm your email before logging in to the system.");
+                return View(model);
+            }
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
@@ -97,6 +104,8 @@ namespace Testv3.Controllers
                     ModelState.AddModelError("", "Invalid login attempt.");
                     return View(model);
             }
+
+            
         }
 
         //
