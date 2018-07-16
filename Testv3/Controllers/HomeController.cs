9d9f61a97ca8b911cb17a4b0bdacf299740d5cd4
Alyssa Bala <Alyssa Bala@MAJBALA>
2018-07-16 10:49:37 +0800

    Make inventory record required for students

--- Testv3/Controllers/HomeController.cs
+++ Testv3/Controllers/HomeController.cs
@@ -10,28 +10,10 @@ using PagedList;
 
 namespace Testv3.Controllers
 {
-
     [Authorize]
     public class HomeController : DefaultController
     {
         private Testv3Entities db = new Testv3Entities();
-
-        //public void GetCurrentUserInViewBag()
-        //{
-        //    var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
-        //    var currentuser = manager.FindById(User.Identity.GetUserId());
-        //    var currentUserId = User.Identity.GetUserId();
-
-        //    var u = db.Students.FirstOrDefault(d => d.UserID == currentUserId);
-
-        //    if (u != null)
-        //    {
-        //        ViewBag.CurrentUser = u.StudentFirstName.Trim() + " " + " " + u.StudentLastName.Trim();
-        //        ViewBag.CurrentUserStudentID = u.StudentID.Trim();
-        //    }
-            
-
-        //}
 
         public ActionResult Index(int page = 1, int recordsPerPage = 5)
         {
@@ -43,60 +25,20 @@ namespace Testv3.Controllers
 
             var list = db.Announcements.ToList().ToPagedList(page, recordsPerPage);
 
-            var students = db.IndividualInventoryRecord
-                    .OrderBy(x => x.RecordID)
-                    .ToList();
-
-
-            foreach (var item in students)
-            {
-
                 var check = db.IndividualInventoryRecord
-                    .Where(x => x.RecordID == item.RecordID && x.UserID == currentUserId && x.UserID == currentUserId)
+                    .Where(x => x.UserID == currentUserId && x.HowStudieIssFinanced != null && x.CourseChoiceInfluence != null )
                     .ToList();
 
                 if (check.Count() == 0)
                 {
-                    TempData["NoIndividualRecord"] = "Please complete your Individual record!";
-                    return RedirectToAction("IndividualRecord", "StudentInventory");
-                }
+                TempData["NoIndividualRecord"] = "Please complete your Individual record!";
+                ViewBag.NoRecord = "Please complete your Individual record!";
             }
+
 
             return View(list);
         }
 
-        public ActionResult ManageRecords()
-        {
-            ViewBag.Message = "Manage student records.";
-            GetCurrentUserInViewBag();
-
-            return View();
-        }
-
-        public ActionResult Appointments()
-        {
-            ViewBag.Message = "View and manage your appointments.";
-            GetCurrentUserInViewBag();
-
-            return View();
-        }
-
-        public ActionResult Reports()
-        {
-            ViewBag.Message = "View and generate available reports.";
-            GetCurrentUserInViewBag();
-
-            return View();
-
-        }
-
-        public ActionResult ManageAccounts()
-        {
-            ViewBag.Message = "Manage user accounts.";
-            GetCurrentUserInViewBag();
-
-            return View();
-
-        }
+        
     }
 }
